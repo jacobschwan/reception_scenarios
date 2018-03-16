290f81217fa19576c43b0fbcf92029549cb6fbf3
Jacob Schwan <blackbrass88@gmail.com>
2018-03-16 17:44:34 -0500

    all items added

--- app.R
+++ app.R
@@ -20,11 +20,16 @@ ui <- fluidPage(
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-         sliderInput("dessert_display",
-                     "Dessert Displays:",
+         sliderInput("hosted",
+                     "Hours of Hosted Booze:",
                      min = 0,
-                     max = 20,
-                     value = 5),
+                     max = 5,
+                     value = 4),
+         sliderInput("na_bev",
+                     "Hosted NA Bevs:",
+                     min = 0,
+                     max = 300,
+                     value = 300),
          sliderInput("chips",
                      "Tortilla Chips:",
                      min = 0,
@@ -39,6 +44,11 @@ ui <- fluidPage(
                      "Trail Mix:",
                      min = 0,
                      max = 10,
+                     value = 5),
+         sliderInput("dessert_display",
+                     "Dessert Displays:",
+                     min = 0,
+                     max = 20,
                      value = 5),
          sliderInput("donuts",
                      "Donuts:",
@@ -64,13 +74,24 @@ ui <- fluidPage(
                      "Mac & Cheese:",
                      min = 0,
                      max = 300,
-                     value = 150)
+                     value = 150),
+         sliderInput("coffee",
+                     "Coffee:",
+                     min = 0,
+                     max = 5,
+                     value = 1),
+         sliderInput("tea",
+                     "Tea:",
+                     min = 0,
+                     max = 5,
+                     value = 1)
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
-         tableOutput("item_list"),
-         tableOutput("scenario_summary")
+          tableOutput("scenario_summary"),
+          tableOutput("item_list")
+         
       )
    )
 )
@@ -79,9 +100,22 @@ ui <- fluidPage(
 server <- function(input, output) {
    
     reception <- gs_title("Reception Food/Bev")
-    food <- gs_read(reception, ws = "food")
+    food <- gs_read(reception, ws = "unit_based")
+    bev <- gs_read(reception, ws = "hours_based")
     
-    output$item_list <- renderTable({
+    bev_cost <- reactive({
+        units_bev <- c(input$hosted)
+        
+        bev %>%
+            mutate(units = units_bev,
+                   people_served = 300) %>%
+            mutate(base_cost = first_hour * people_served + (units -1 ) * second_hour * people_served) %>%
+            mutate(sc_cost = (base_cost * .22) * 1.0753,
+                   base_tax = base_cost * tax) %>%
+            mutate(total_cost = base_cost + sc_cost + base_tax)
+    })
+    
+    item_list <- reactive({
         units_vec <- c(input$dessert_display,
                        input$chips,
                        input$nuts,
@@ -90,39 +124,33 @@ server <- function(input, output) {
                        input$pizza,
                        input$tenders,
                        input$tots,
-                       input$mac)
+                       input$mac,
+                       input$coffee,
+                       input$tea,
+                       input$na_bev)
+        
+        drink <- bev_cost() %>%
+            select(item, units, unit, total_cost, people_served, category)
         
         food %>%
             mutate(units = units_vec) %>%
             mutate(base_cost = units * unit_price) %>%
             mutate(sc_cost = (base_cost * .22) * 1.0753,
-                   base_tax = base_cost * 1.0753) %>%
+                   base_tax = base_cost * 0.0753) %>%
             mutate(total_cost = base_cost + sc_cost + base_tax,
                    people_served = people * units) %>%
-        select(item, units, total_cost, people_served)})
+        select(item, units, unit, total_cost, people_served, category) %>%
+            bind_rows(drink)})
+    
+    output$item_list <- renderTable({item_list() %>%
+            select(-category)})
     
     output$scenario_summary <- renderTable({
-        units_vec <- c(input$dessert_display,
-                       input$chips,
-                       input$nuts,
-                       input$t_mix,
-                       input$donuts,
-                       input$pizza,
-                       input$tenders,
-                       input$tots,
-                       input$mac)
-        
-        food %>%
-            mutate(units = units_vec) %>%
-            mutate(base_cost = units * unit_price) %>%
-            mutate(sc_cost = (base_cost * .22) * 1.0753,
-                   base_tax = base_cost * 1.0753) %>%
-            mutate(total_cost = base_cost + sc_cost + base_tax,
-                   people_served = people * units) %>%
+        item_list() %>%
         group_by(category) %>%
         summarise(total_cost = sum(total_cost),
                   people_served = sum(people_served, na.rm = T)) %>%
-        janitor::add_totals_row()})
+        janitor::adorn_totals("row")})
  
 }
 
